abd8fecfad2e03c6959f759bb9a3b2f3d6787e33
Anieyo0 <106874538+Anieyo0@users.noreply.github.com>
2022-12-09 20:57:58 +0900

    Update main.java

--- 1/main.java
+++ 1/main.java
@@ -2,13 +2,13 @@ public class main {
     public static void main(String[] args) {
     
     // Fighter 객체를 하나 만들고, 
-    Fighter figher1 = new Warrior();
+    Fighter fighter1 = new Warrior();
     fighter1.attack();
     fighter1.defend();
     fighter1.escape();
 
     // WizardAdapter 사용
-    Fighter figher2 = new WizardAdapter();
+    Fighter fighter2 = new WizardAdapter();
     fighter2.attack();
     fighter2.defend(); 
     fighter2.escape();
