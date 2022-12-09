20bd7e487c110f93beef3920c01f8438dbfc8840
Anieyo0 <106874538+Anieyo0@users.noreply.github.com>
2022-12-09 21:18:55 +0900

    Update WizardAdapter.java

--- 1/WizardAdapter.java
+++ 1/WizardAdapter.java
@@ -1,21 +1,19 @@
 public class WizardAdapter extends Fighter {
-    
+
+	Wizard wizard = new Wizard();
     public WizardAdapter(){
-        System.out.println('직업 : 마법사');
+        System.out.println("직업 : 마법사");
     }
 
     public void attack(){
-        Wizard wizard = new Wizard();
         wizard.shot_fire_ball();
     }
 
     public void defend(){
-        Wizard wizard = new Wizard();
         wizard.shield();
     }
 
     public void escape(){
-        Wizard wizard = new Wizard();
         wizard.portal();
     }
 }
